412b3107f4d0f53fb89f1539021d202bf53c640e
Vinhbb96er <vinhbb96er@gmail.com>
2018-02-27 13:29:29 +0700

    init-migration

---
+++ database/migrations/2018_02_27_123949_add_columns_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('description')->after('password');
+            $table->string('avatar')->after('description');
+            $table->boolean('is_admin')->after('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('description');
+            $table->dropColumn('avatar');
+            $table->dropColumn('is_admin');
+        });
+    }
+}
